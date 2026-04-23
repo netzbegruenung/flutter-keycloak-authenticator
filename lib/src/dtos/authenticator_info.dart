7119c8945672ab871621f23fb629200040cd4171
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-04-23 13:41:27 +0200

    Add flutter keycloak authenticator

---
+++ lib/src/dtos/authenticator_info.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_keycloak_authenticator/src/enums/key_algorithm_enum.dart';
+import 'package:flutter_keycloak_authenticator/src/enums/signature_algorithm_enum.dart';
+
+class AuthenticatorInfo {
+  final SignatureAlgorithm signatureAlgorithm;
+  final KeyAlgorithm keyAlgorithm;
+  final DateTime registeredAt;
+
+  AuthenticatorInfo({required this.signatureAlgorithm, required this.keyAlgorithm, required this.registeredAt});
+}
